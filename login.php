b0df2f174146e5d89d435a051cd5ba1d2d0f232d
John <john@smorz.cs.yale.edu.fake>
2013-09-03 14:55:29 -0400

    login update

--- login.php
+++ login.php
@@ -4,8 +4,9 @@
 <form action="wiki/api.php" method="post">
 
 Action: <input type="text" name="action" value="login"><br>
-Name: <input type="text" name="lgname"><br>
+Name: <input type="text" name="lgname" value="Bryan"><br>
 Password: <input type="text" name="lgpassword"><br>
+Token: <input type="text" name="lgtoken"><br>
 
 <input type="submit">
 </form>
