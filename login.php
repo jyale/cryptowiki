3e93d87555dfbfec50013ede6a65973f541f80a4
John <john@smorz.cs.yale.edu.fake>
2013-09-03 14:45:13 -0400

    basis for login

---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+<form action="wiki/api.php" method="post">
+
+Action: <input type="text" name="action" value="login"><br>
+Name: <input type="text" name="lgname"><br>
+Password: <input type="text" name="lgpassword"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html> 
